e289751eea691447a76cb20491bcdd6b194ac4f0
Daniel Waite <danielwaite6@hotmail.com>
2022-01-20 17:37:36 -0300

    second commit

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn } from 'next-auth/react';
 import React from 'react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 import styles from './styles.module.scss';
 
@@ -11,12 +13,20 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     const { data: session } = useSession()
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
         }
 
+        try {
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data
+            const stripe = await getStripeJs()
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            alert(err.message)
+        }
 
     }
 
