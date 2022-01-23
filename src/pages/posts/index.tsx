1cea34e231de783b417760777faef804ed582f4a
Daniel Waite <danielwaite6@hotmail.com>
2022-01-23 20:56:18 -0300

    second commit

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -29,7 +29,7 @@ export default function Posts({ posts }: PostProps) {
                 <div className={styles.posts}>
                     {posts.map(post => (
                         <Link href={`/posts/${post.slug}`} key={post.slug}>
-                            <a>
+                            <a key={post.slug}>
                                 <time>{post.updatedAt}</time>
                                 <strong>{post.title}</strong>
                                 <p>{post.excerpt}</p>
