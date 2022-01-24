8ff2cdaeb5dfcab80bf68cb331f2bb4aef16131d
Daniel Waite <danielwaite6@hotmail.com>
2022-01-24 14:40:31 -0300

    second commit

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,8 +38,19 @@ export default function Post({ post }: PostProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
 
-    const session = await getSession({ req });
     const { slug } = params;
+
+    const session = await getSession({ req });
+
+    if (!session?.activeSubscription) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
 
     const prismic = getPrismicClient(req)
 
