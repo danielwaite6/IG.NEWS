f099cfd6381612ca8ec980f568ff21d4aeb688f1
Daniel Waite <danielwaite6@hotmail.com>
2022-01-26 14:55:44 -0300

    first commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -87,6 +87,7 @@ export default NextAuth({
                 )
                 return true
             } catch (error) {
+                console.log('ERROR: ', error)
                 return false
             }
 
